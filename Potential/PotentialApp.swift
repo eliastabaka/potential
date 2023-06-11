32d5a826d8ce3ff123a0c28d04897b964206f561
Elias Tabaka <mmtabaka1@sheffield.ac.uk>
2023-06-12 00:34:38 +0100

    Initial Commit

---
+++ Potential/PotentialApp.swift
@@ -0,0 +1,17 @@
+//
+//  PotentialApp.swift
+//  Potential
+//
+//  Created by Elias Tabaka on 12/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct PotentialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
